dff35f710fa31dd185b688370c42beb05bb17462
Kabir <kabirajibade@yahoo.com>
2023-05-20 15:09:49 +0100

    local storage now storing the registration detail snd blog details

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -1,22 +1,49 @@
-
+import {useState} from 'react'
 
 function Admin() {
+  const [blog,setBlog]=useState({
+   Title:"",
+    Blog:""
+  })
+  const [error,setError]=useState([])
+  const addBlog=(e)=>{
+    e.preventDefault()
+    if(blog.Title==""){
+      setError((err)=>{
+        return{...err,TitleErr:'Title cannot be blank'}
+      })
+    }
+    if(blog.Blog==""){
+      setError((err)=>{
+        return {...err, BlogErr:'Blog cannot be empty'}
+      })
+    }
+    if(error.length==0){
+      const blogDetails=JSON.stringify(blog)
+      localStorage.setItem(blog,blogDetails)
+      console.log('Blog saved successfully!')
+    }
+  }
   return (
     <div id="adminPage">
     <h1 className="banner">Admin</h1>
-   
-    <section className="d-flex justify-content-center">
+    {/* <h5>{JSON.stringify(blog)}</h5> */}
+   {/* <h5>{JSON.stringify(error)}</h5> */}
+    <section className="d-flex justify-content-center" onSubmit={addBlog}>
         <form action="" method="post" className="attributecolor text-bg-light form-control-md w-50">
          
           <div className="my-3 p-3">
-            <label htmlFor="Email" className="mx-2">Title</label>
-            <input type="text" name="Email" id="Email" className="form-control-sm text-bg-light"/>
+            <label htmlFor="Title" className="mx-2">Title</label>
+            <input type="text" name="Title" id="Title" className="form-control-sm text-bg-light" onChange={(e)=>{
+              setBlog({...blog,Title:e.target.value})
+            }}/>
           </div>
 
           <div className="my-3 p-3">
-            <label htmlFor="blogNews" className="mx-2">News</label> <br />
-          <textarea name="blogNews" id="blogNews" cols="50" rows="15">
-
+            <label htmlFor="Blog" className="mx-2">News</label> <br />
+          <textarea name="Blog" id="Blog" cols="50" rows="15" onChange={(e)=>{
+            setBlog({...blog,Blog:e.target.value})
+          }}>
           </textarea>
           </div>
 
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -2,29 +2,67 @@ import {useState} from 'react'
 function Register() {
   const [user, setUser]=useState({
     Email:"",
+    Phone:"",
     Password:""
-
   })
+  const [errors, setErrors]=useState([])
+  const registerUser=(e)=>{
+    e.preventDefault()
+    if(user.Email==""){
+      setErrors((err)=>{
+        return{...err,EmailErr:'Your Email is is needed'}
+      })
+    }
+    if(user.Password==""){
+      setErrors((err)=>{
+        return {...err,PasswordErr:'Password is required'}
+      })
+    }
+    else if(user.Password.length<6){
+      setErrors((err)=>{
+        return {...err,PasswordErr:'Password must be six(6) or more characters'}
+      })
+    }
+    if(errors.length==0){
+      const allData=JSON.stringify(user)
+      localStorage.setItem(Email,allData)
+      console.log('Saved, No Errors')
+    }
+  }
   return (
     <div>
     
    <div id="registerPage">
    <h1 className="banner">Register</h1>
    <section className="d-flex justify-content-center">
-    <form action="" method="post" className="attributecolor text-bg-light form-control-md w-50" id="register">
+    {/* <h4>{JSON.stringify(user)}</h4> */}
+    {/* <h5>{JSON.stringify(errors)}</h5> */}
+    <form action="" method="post" onSubmit={registerUser} className="attributecolor text-bg-light form-control-md w-50" id="register">
          
           <div className="my-3 p-3">
             <label htmlFor="Email" className="mx-4">Email</label>
-            <input type="text" name="Email" id="Email" className="form-control-sm text-bg-light"/>
+            <input type="text" name="Email" id="Email" className="form-control-sm text-bg-light"  onChange={(e)=>{
+                setUser({...user,Email:e.target.value})
+            }}/>
+          
+          </div>
+
+          <div className="my-3 p-3">
+            <label htmlFor="Phone" className="mx-4">Phone</label>
+            <input type="text" name="Phone" id="Phone" className="form-control-sm text-bg-light" onChange={(e)=>{
+              setUser({...user,Phone:e.target.value})
+            }}/>
           </div>
 
           <div className="my-3 p-3">
             <label htmlFor="Password" className="mx-4">Password</label>
-            <input type="password" name="Password" id="Password" className="form-control-sm text-bg-light"/>
+            <input type="password" name="Password" id="Password" className="form-control-sm text-bg-light" onChange={(e)=>{
+              setUser({...user,Password:e.target.value})
+            }}/>
           </div>
 
           <div className="my-3 p-3">
-            <label htmlFor="Password" className="mx-4">Confirm Password</label>
+            <label htmlFor="Password" className="mx-2">Confirm Password</label>
             <input type="password" name="Password" id="Password" className="form-control-sm text-bg-light"/>
           </div>
 
